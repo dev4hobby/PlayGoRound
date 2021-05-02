3a978666415e30459fc0f752fbf4001412e54397
Seongchuel <aciddust20@gmail.com>
2021-05-03 00:46:08 +0900

    Update, envvar

--- shop-back/main.go
+++ shop-back/main.go
@@ -2,11 +2,12 @@ package main
 
 import (
 	"log"
+	"os"
 
 	"github.com/dev4hobby/PlayGoRound/shop-back/rest"
 )
 
 func main() {
 	log.Println("Main log...")
-	log.Fatal(rest.RunAPI("127.0.0.1:8000"))
+	log.Fatal(rest.RunAPI(":" + os.Getenv("GO_PORT")))
 }
