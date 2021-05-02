f359d9ef0b0504ecb3e13fbdd8215803d4df410e
Seongchuel <aciddust20@gmail.com>
2021-05-02 16:57:47 +0900

    modify field name

--- shop-back/models/models.go
+++ shop-back/models/models.go
@@ -1,13 +1,14 @@
 package models
 
 import (
-	"github.com/jinzhu/gorm"
 	"time"
+
+	"github.com/jinzhu/gorm"
 )
 
 type Customer struct {
 	gorm.Model
-	Name      string  `json:"name"`
+	Name      string  `json:"name" sql:"-"`
 	FirstName string  `gorm:"column:firstname" json:"firstname"`
 	LastName  string  `gorm:"column:lastname" json:"lastname"`
 	Email     string  `gorm:"column:email" json:"email"`
